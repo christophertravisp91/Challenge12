2685ab586df468a62003369e706c578cc0713014
TomFoolery <christophertravisp91@gmail.com>
2022-01-29 14:18:06 -0500

    changed connection

--- index.js
+++ index.js
@@ -2,16 +2,9 @@ const inquirer = require("inqurer");
 const mysql = require("mysql2");
 const cTable = require("console.table");
 
-let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3005,
-    user: "root",
-    password: "homework",
-    database: "employee_db"
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'employee_db'
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-        console.log("connected as id " + connection.threadId + "/n");
-        runSearch();
-});
