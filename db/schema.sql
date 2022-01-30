ec0a6b2997d4f1c08e95074b3ff7d94d1b619eaf
TomFoolery <christophertravisp91@gmail.com>
2022-01-30 13:42:23 -0500

    fixing db

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE roles(
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2),
-    department_id INTEGER NOT NULL,
+    department_id INTEGER,
     FOREIGN KEY (department_id) REFERENCES departments(id) 
     ON DELETE SET NULL
 );
@@ -24,8 +24,6 @@ CREATE TABLE employee(
     role_id INTEGER NOT NULL,
     manager_id INTEGER,
     FOREIGN KEY (role_id) REFERENCES roles(id) 
-    ON DELETE SET NULL
-    FOREIGN KEY (manager_id references managers(manager_id)
-    ON DELETE SET NULL)
+    ON DELETE SET NULL 
 );
 
