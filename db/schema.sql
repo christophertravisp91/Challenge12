43629af33f85011cd50fe8cec44ea54e966631a1
TomFoolery <christophertravisp91@gmail.com>
2022-01-30 13:50:12 -0500

    db errors

--- db/schema.sql
+++ db/schema.sql
@@ -4,26 +4,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department(
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     dept_name VARCHAR(30) NOT NULL,
 );
 
 CREATE TABLE roles(
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2),
     department_id INTEGER,
-    FOREIGN KEY (department_id) REFERENCES department(id) 
-    ON DELETE SET NULL
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee(
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER NOT NULL,
     manager_id INTEGER,
-    FOREIGN KEY (role_id) REFERENCES roles(id) 
-    ON DELETE SET NULL 
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL 
 );
 
