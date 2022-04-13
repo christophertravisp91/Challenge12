fb8509edd5acf02c52b23096689cea5800f9a076
TomFoolery <christophertravisp91@gmail.com>
2022-04-13 12:15:09 -0400

    tried to update role

--- server.js
+++ server.js
@@ -55,9 +55,6 @@ function options() {
                     case 'Update employee role':
                         updateRole();
                         break;
-                    case 'Delete an employee':
-                        deleteEmployee();
-                        break;
                     case 'EXIT': 
                         exitApp();
                         break;
@@ -229,8 +226,63 @@ function addRole() {
 };
 
 function updateRole() {
-
-};
+    var query = 'SELECT * FROM employee';
+    connection.query(query, (err, emplRes) => {
+        if (err) throw err;
+        const employeeChoice = [];
+        emplRes.forEach(({ first_name, last_name, id }) => {
+            employeeChoice.push({
+            name: first_name + " " + last_name,
+            value: id
+            });
+        });
+        
+        //get all the role list to make choice of employee's role
+        connection.query("SELECT * FROM ROLE", (err, rolRes) => {
+            if (err) throw err;
+            const roleChoice = [];
+            rolRes.forEach(({ title, id }) => {
+            roleChoice.push({
+                name: title,
+                value: id
+                });
+            });
+        
+        let questions = [
+            {
+                type: "list",
+                name: "id",
+                choices: employeeChoice,
+                message: "whose role do you want to update?"
+            },
+            {
+                type: "list",
+                name: "role_id",
+                choices: roleChoice,
+                message: "what is the employee's new role?"
+            }
+            ]
+    
+        inquier.prompt(questions)
+            .then(response => {
+                const query = `UPDATE EMPLOYEE SET ? WHERE ?? = ?;`;
+                connection.query(query, [
+                {role_id: response.role_id},
+                "id",
+                response.id
+                ], (err, res) => {
+                if (err) throw err;
+                
+                console.log("successfully updated employee's role!");
+                startPrompt();
+                });
+            })
+            .catch(err => {
+                console.error(err);
+            });
+            })
+        });
+    };
 
 function exitApp() {
     connection.end();
